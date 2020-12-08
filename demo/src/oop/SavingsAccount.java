313c43bb3d46ca46d19fb04f135357392f00368b
srikanthpragada <srikanthpragada@gmail.com>
2020-12-08 18:52:03 +0530

    08-DEC

--- demo/src/oop/SavingsAccount.java
+++ demo/src/oop/SavingsAccount.java
@@ -5,22 +5,33 @@ public class SavingsAccount {
 	private int acno;
 	private String ahname;
 	private double balance;
-	
-	// Methods 
-	public void open(int ano, String name) {
+
+	// Constructor
+	public SavingsAccount(int ano, String name) {
 		acno = ano;
 		ahname = name;
 		balance = 0;
 	}
-	
+
+	public SavingsAccount(int ano, String name, double bal) {
+		acno = ano;
+		ahname = name;
+		balance = bal;
+	}
+
+	// Methods
 	public void print() {
 		System.out.println(acno);
 		System.out.println(ahname);
 		System.out.println(balance);
 	}
-	
+
 	public void deposit(double amount) {
 		balance += amount;
+	}
+
+	public double getBalance() {
+		return balance;
 	}
 
 }
--- demo/src/oop/TestSavingsAccount.java
+++ demo/src/oop/TestSavingsAccount.java
@@ -4,14 +4,15 @@ public class TestSavingsAccount {
 
 	public static void main(String[] args) {
 		SavingsAccount a; // object reference
-
-		a = new SavingsAccount(); // object
-		a.open(1, "Scott");
+ 
+		a = new SavingsAccount(1, "Scott");
 		a.deposit(10000);
 		a.print();
 		
-		SavingsAccount a2 = new SavingsAccount();
-		a2.print();
+		SavingsAccount a2 = new SavingsAccount(2,"Tom",20000);  
+		
+		
+	 
 	}
 
 }
