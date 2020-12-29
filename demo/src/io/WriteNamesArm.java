7ba0775816ddce72a2333b20c945bca24d2610d9
srikanthpragada <srikanthpragada@gmail.com>
2020-12-29 18:55:58 +0530

    29-DEC

---
+++ demo/src/io/WriteNamesArm.java
@@ -0,0 +1,21 @@
+package io;
+
+import java.io.FileWriter;
+
+public class WriteNamesArm {
+
+	public static void main(String[] args) {
+		// Try with resources 
+		try (FileWriter fw = new FileWriter("names.txt")) {
+
+			String names[] = { "Java", "JavaScript", "Python", "C#", "TypeScript", "C" };
+
+			for (String name : names)
+				fw.write(name + "\n");
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+	}
+
+}
